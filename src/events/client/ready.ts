75c5cb826d2237c1f76b203527fee9eeaad86300
Jacob Bolden <jlbolden.pro@gmail.com>
2022-05-19 12:53:15 -0500

    positioning

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -7,9 +7,9 @@ export default {
   async execute(client: Client) {
     // const { commands } = await import("../../ignore/commands");
     // const buds = client.guilds.cache.get(process.env.BUDS);
-    // await buds?.commands.set(commands).catch((e) => console.log(e));
+    // await buds?.commands.set([]).catch((e) => console.log(e));
+    // await client.application?.commands.set(commands).catch((e) => console.log(e));
     // useLog("READY", async () => {
-    // await client.application?.commands.set([]).catch((e) => console.log(e));
     // const ar = await buds?.commands.edit("970755495382106192", {
     //   name: "test",
     //   description: "test command",
