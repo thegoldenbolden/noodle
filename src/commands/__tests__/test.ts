20b36ecc3911840153bf21f0c44b38dbebe63315
Jacob Bolden <jlbolden.pro@gmail.com>
2023-02-16 15:02:56 -0600

    removed test

--- src/commands/__tests__/test.ts
+++ src/commands/__tests__/test.ts
@@ -1,25 +1,10 @@
-import {
- ComponentType,
- MentionableSelectMenuComponentData,
- RoleSelectMenuBuilder,
- RoleSelectMenuComponentData,
-} from "discord.js";
-import { Command } from "../../types";
+import type { Command } from "../../types";
 
 export default {
  name: "test",
  categories: ["Miscellaneous"],
  cooldown: 10,
  execute: async (interaction) => {
-  await interaction.deferReply();
-
-  const roleSelectMenu: MentionableSelectMenuComponentData = {
-   customId: "AUTOROLE",
-   placeholder: "Select a role",
-   type: ComponentType.MentionableSelect,
-   maxValues: 25,
-  };
-
-  await interaction.editReply({});
+  await interaction.editReply("No tests");
  },
 } as Command;
